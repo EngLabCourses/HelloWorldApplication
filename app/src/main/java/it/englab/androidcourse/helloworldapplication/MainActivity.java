45c0afcce91ec792222c69824a137fe98a202e64
spawn150 <spawn150@gmail.com>
2017-01-26 17:28:11 +0100

    modified log message using string resource

--- app/src/main/java/it/englab/androidcourse/helloworldapplication/MainActivity.java
+++ app/src/main/java/it/englab/androidcourse/helloworldapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String logMessage = "Hello World!";
+        String logMessage = getString(R.string.hello_world_text);
         Log.d(TAG, logMessage);
     }
 }
