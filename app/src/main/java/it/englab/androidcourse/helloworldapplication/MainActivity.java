137ea399ff91804ff93ea8b934e40aff7a99115b
spawn150 <spawn150@gmail.com>
2017-02-06 23:20:25 +0100

    aggiunto esempio per BroadcastReceiver

--- app/src/main/java/it/englab/androidcourse/helloworldapplication/MainActivity.java
+++ app/src/main/java/it/englab/androidcourse/helloworldapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package it.englab.androidcourse.helloworldapplication;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,6 +9,7 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
+    private MyBroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String logMessage = getString(R.string.hello_world_text);
         Log.d(TAG, logMessage);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        receiver = new MyBroadcastReceiver();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(receiver);
+        super.onPause();
     }
 }
---
+++ app/src/main/java/it/englab/androidcourse/helloworldapplication/MyBroadcastReceiver.java
@@ -0,0 +1,26 @@
+package it.englab.androidcourse.helloworldapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by spawn on 06/02/17.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String TAG = MyBroadcastReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+            Log.d(TAG, "ACTION_BOOT_COMPLETED");
+        } else if (Intent.ACTION_AIRPLANE_MODE_CHANGED.equals(action)) {
+            Log.d(TAG, "ACTION_AIRPLANE_MODE_CHANGED");
+        }
+    }
+
+}
