04914636ee3088e2b76d7016e5149656aa0f9dfe
spawn150 <spawn150@gmail.com>
2017-02-08 16:00:45 +0100

    rimosso LeakCanary ed esempio di memory Leak

--- app/src/main/java/it/englab/androidcourse/helloworldapplication/MyApplication.java
+++ app/src/main/java/it/englab/androidcourse/helloworldapplication/MyApplication.java
@@ -2,8 +2,6 @@ package it.englab.androidcourse.helloworldapplication;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 /**
  * Created by spawn on 05/02/17.
  */
@@ -13,12 +11,5 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
     }
 }
